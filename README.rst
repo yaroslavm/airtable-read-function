f4c5fb528fcec4a9f6b41dc6d48f551fe47ffa87
Yaroslav Matveychuk <yaroslav.matveychuk@gmail.com>
2022-04-10 20:29:23 +0300

    draft

---
+++ README.rst
@@ -0,0 +1,22 @@
+======================
+Airtable read function
+======================
+
+Google cloud function to read records from Airtable and push them into a topic one by one.
+
+
+* Free software: MIT license
+
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This function was created with Cookiecutter_ and the `yaroslavm/cookiecutter-gcp-function-java`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`yaroslavm/cookiecutter-gcp-function-java`: https://github.com/yaroslavm/cookiecutter-gcp-function-java
---
+++ src/main/java/functions/AirtableReadFunction.java
@@ -0,0 +1,32 @@
+package functions;
+
+import com.google.cloud.functions.BackgroundFunction;
+import com.google.cloud.functions.Context;
+import com.google.events.cloud.pubsub.v1.Message;
+import com.google.gson.Gson;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AirtableReadFunction implements BackgroundFunction<Message> {
+  private static final Gson gson = new Gson();
+
+  @Override
+  public void accept(final Message message, final Context context) throws Exception {
+    log.info(
+        "Message arrived with id: {}, publishTime: {}, attributes: {}, context: {}",
+        message.getMessageID(),
+        message.getPublishTime(),
+        message.getAttributes(),
+        context);
+    if (message.getData() == null) {
+      log.warn("field `data` is null, exiting.");
+      return;
+    }
+    final var rawDataDecoded =
+        new String(Base64.getDecoder().decode(message.getData()), StandardCharsets.UTF_8);
+    final var dataObj = gson.fromJson(rawDataDecoded, Object.class);
+    log.info("dataObj received: {}", dataObj);
+  }
+}
